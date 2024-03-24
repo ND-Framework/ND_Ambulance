e472741328da95784a713510612c65c298aee28b
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2024-03-24 18:44:05 +0100

    tweak(data/medbag): add bandage

--- data/medbag.lua
+++ data/medbag.lua
@@ -2,5 +2,6 @@ return { -- items included in a medbag, item. Name and amount.
     {"tourniquet", 4},
     {"gauze", 15},
     {"burndressing", 15},
-    {"splint", 3}
+    {"splint", 3},
+    {"bandage", 5}
 }
